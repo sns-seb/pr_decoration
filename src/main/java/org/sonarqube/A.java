17ba191b9588adb6f80c9db0f10d1c53c2ee808b
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-12-14 17:20:52 +0100

    add method to be made unused later

--- src/main/java/org/sonarqube/A.java
+++ src/main/java/org/sonarqube/A.java
@@ -4,7 +4,27 @@ public class A {
   String message = "foo";
 
   public String foo() {
+    if (bar() < doh()) {
+      unused1();
+      lastBarBeforeTheEndOfTime();
+    }
     return message;
+  }
+
+  public Integer bar() {
+    return 42;
+  }
+
+  public double doh() {
+    return 12.5;
+  }
+
+  private static String unused1() {
+    return "will_be_unused";
+  }
+
+  private int lastBarBeforeTheEndOfTime() {
+    return -666;
   }
 
 }
