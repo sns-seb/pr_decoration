ecc12de5955fb570695c5ac0c2e5fd2c1e40c577
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-04-05 16:14:15 +0200

    WIP update scan_pr.sh to use PR support in SQ

--- scan_pr.sh
+++ scan_pr.sh
@@ -6,4 +6,9 @@ BRANCH="$1"
 PR_ID="$2"
 
 git checkout $BRANCH
-$M2_HOME/bin/mvn sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=admin -Dsonar.password=admin -Dsonar.scm.provider=git -Dsonar.pullrequest.github.repository=sns-seb/pr_decoration -Dsonar.branch.name=$BRANCH -Dsonar.pullrequest.github.id=$PR_ID
+#$M2_HOME/bin/mvn sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=admin -Dsonar.password=admin -Dsonar.scm.provider=git -Dsonar.pullrequest.github.repository=sns-seb/pr_decoration -Dsonar.branch.name=$BRANCH -Dsonar.pullrequest.github.id=$PR_ID
+$M2_HOME/bin/mvn sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=admin -Dsonar.password=admin -Dsonar.scm.provider=git -Dsonar.pullrequest.key=$PR_ID \
+        -Dsonar.pullrequest.branch=$BRANCH \
+        -Dsonar.pullrequest.base=master \
+        -Dsonar.pullrequest.provider=github \
+        -Dsonar.pullrequest.github.repository=sns-seb/pr_decoration
